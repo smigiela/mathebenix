66827391d4d169ac8034a2e04351f67a3d08c358
Daniel <daniel.smigiela@outlook.com>
2018-01-28 14:25:38 +0100

    dodano pętle for

--- src/com/company/addition.java
+++ src/com/company/addition.java
@@ -8,19 +8,22 @@ public class addition {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner scanner = new Scanner(System.in);
-        Random r = new Random();
-        int a = r.nextInt(99)+1;
-        int b = r.nextInt(99)+1;
-        int suma = a+b;
-        System.out.println(a+" + "+b+" = ");
-        int wynik = scanner.nextInt();
-        System.out.println(a+" + "+b+" = "+wynik);
-        if (wynik == suma) {
-            System.out.println("Zgadłaś, brawo!");
-        } else {
-            System.out.println("Gapa z Ciebie! Prawidłowy wynik to"+" "+suma);
+        for (int i=0; i<10; i++){
+            Scanner scanner = new Scanner(System.in);
+            Random r = new Random();
+            int a = r.nextInt(99)+1;
+            int b = r.nextInt(99)+1;
+            int suma = a+b;
+            System.out.println(a+" + "+b+" = ");
+            int wynik = scanner.nextInt();
+            System.out.println(a+" + "+b+" = "+wynik);
+            if (wynik == suma) {
+                System.out.println("Zgadłaś, brawo!");
+            } else {
+                System.out.println("Gapa z Ciebie! Prawidłowy wynik to"+" "+suma);
+            }
         }
+
 
 
 
